a76d275acc707542c60ef47f8bd3c4ab9ade9a85
Alexander Konopelski <22konopelskialexande@gmail.com>
2025-06-02 11:32:07 -0400

    Add start script to package.json for Heroku

--- models/Satisfaction.js
+++ models/Satisfaction.js
@@ -5,8 +5,8 @@ const satisfactionSchema = new mongoose.Schema({
   level: {
     type: Number,
     required: true,
-    min: 1, // e.g., 1 for very unsatisfied
-    max: 5  // e.g., 5 for very satisfied
+    min: 1,
+    max: 5
   },
   comment: {
     type: String,
