e82d28e26af6723bbbb365940803d21b8e2eb29d
Alexander Konopelski <22konopelskialexande@gmail.com>
2025-06-05 10:22:12 -0400

    moved slider css outside of media

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
